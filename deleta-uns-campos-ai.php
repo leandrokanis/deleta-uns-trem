983ff5e6e642d2a4e304f06355e1511d5c305556
Leandro Alves <leandrosustenido@gmail.com>
2016-11-09 11:43:59 -0200

    add filter on billing fields

---
+++ deleta-uns-campos-ai.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package Hello_Dolly
+ * @version 0.1
+ */
+/*
+Plugin Name: Hello Dolly
+Plugin Name: Deleta uns campos ai
+Plugin URI: https://leandro.cf
+Description: Deleta uns campos ai
+Author: Leandro Alves
+Version: 0.1
+Author URI: http://leandro.cf
+*/
+
+
+// Hook in
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+
+// Our hooked in function - $fields is passed via the filter!
+function custom_override_checkout_fields( $fields ) {
+     unset($fields['order']['order_comments']);
+     unset($fields['billing']['billing_last_name']);
+     unset($fields['billing']['billing_country']);
+     unset($fields['billing']['billing_company']);
+     unset($fields['billing']['billing_phone']);
+     unset($fields['billing']['billing_postcode']);
+     unset($fields['billing']['billing_state']);
+
+     return $fields;
+}
+
+
+add_action( 'get_header', 'custom_override_checkout_fields' );
+
+?>
